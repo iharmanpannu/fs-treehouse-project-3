32e8836b70f6a87743a77e59aa93f5bf0f10c5c0
iharmanpannu <iharmanpannu@gmail.com>
2018-02-08 01:07:07 -0800

    adding another function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 const form = document.querySelector("form");
 const name = document.querySelector("#name");
+const firstFieldSet = document.getElementsByName("fieldset");
 
 window.addEventListener("load", () => {
   focusNameInput();
+  otherJobTextField();
 });
 
 // This function adds focus when page loads
 function focusNameInput() {
   name.focus();
+}
+
+function otherJobTextField() {
+  const textArea = document.createElement("input");
+  textArea.setAttribute("type", "text");
+  textArea.id = "other-title";
+  textArea.setAttribute("placeholder", "Your Job Role");
+  // '<input type="text"  id="other-title" placeholder="Your Job Role">';
+  firstFieldSet.appendChild(textArea);
+  return;
 }
