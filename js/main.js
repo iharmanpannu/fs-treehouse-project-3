4fa9a9b2febc2e9982b2ba6ed73ea9f62c93432b
iharmanpannu <iharmanpannu@gmail.com>
2018-02-12 12:21:53 -0800

    adding show colors function

--- js/main.js
+++ js/main.js
@@ -3,10 +3,8 @@ const name = document.querySelector("#name");
 const JobRoles = document.querySelector("select");
 const firstFieldSet = document.querySelectorAll("fieldset")[0];
 const InputOther = document.querySelector("#other-title");
-InputOther.style.display = "none";
-// const optionOtherJob = document.querySelectorAll("option");
-// const inputOption = document.getElementById("other-title");
-// inputOption.style.display = "none";
+const designSelectMenu = document.querySelector("#design");
+const colorOptions = document.querySelectorAll("#color option");
 
 window.addEventListener("load", () => {
   focusNameInput();
@@ -18,6 +16,7 @@ function focusNameInput() {
 }
 
 // Other Job selected function
+InputOther.style.display = "none";
 function onOtherJobSelected(e) {
   // if in job Selection select menu option other job is onOtherJobSelected
   // then display then text box else display nothing
@@ -28,3 +27,15 @@ function onOtherJobSelected(e) {
   InputOther.focus();
 }
 JobRoles.onchange = onOtherJobSelected;
+
+//change colors as the user selects the menu
+// Hide all the options
+for (let i = 0; i < colorOptions.length; i++) {
+  colorOptions[i].style.display = "none";
+}
+function showAvailableColors(e) {
+  // if theme js pune selected then show "cornflower blue, Dark slate grey, and gold "
+  // if theme I JS selected show Tomoto, steel blue and dim grey
+  // if (e.target.value === 'js pune')
+}
+designSelectMenu.onchange = showAvailableColors;
