a9961548d87410759a61ee8ba9f2ddfe21be954c
iharmanpannu <iharmanpannu@gmail.com>
2018-02-12 00:41:12 -0800

    adding event

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const form = document.querySelector("form");
 const name = document.querySelector("#name");
 const firstFieldSet = document.querySelectorAll("fieldset")[0];
+const inputOption = document.querySelector("#other-title");
+// inputOption.style.display = "none";
 
 window.addEventListener("load", () => {
   focusNameInput();
+  ChangedToOther();
 });
 
 // This function adds focus when page loads
@@ -11,13 +14,19 @@ function focusNameInput() {
   name.focus();
 }
 
-function appendInputOtherJob() {
-  const otherJobInput = document.createElement("input");
-  otherJobInput.setAttribute("type", "text");
-  otherJobInput.id = "other-title";
-  otherJobInput.setAttribute("placeholder", "Your Job Role");
-  otherJobInput.style.display = "none";
-  firstFieldSet.appendChild(otherJobInput);
-}
+// function appendInputOtherJob(event) {
+//   const otherJobInput = document.createElement("input");
+//   otherJobInput.setAttribute("type", "text");
+//   otherJobInput.id = "other-title";
+//   otherJobInput.setAttribute("placeholder", "Your Job Role");
+//   otherJobInput.style.display = "none";
+//   firstFieldSet.appendChild(otherJobInput);
 
-appendInputOtherJob();
+// }
+
+function ChangedToOther(event) {
+  //if the job role event was changed to other a nice text menu is created
+  event.target.value == "other"
+    ? (inputOption.style.display = "block")
+    : (inputOption.style.display = "none");
+}
