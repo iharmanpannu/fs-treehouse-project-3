e16ce9ceb05add2a5d351975dcb79fd84191efd6
iharmanpannu <iharmanpannu@gmail.com>
2018-02-14 19:17:53 -0800

    fixed the issue with colors

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ const JobRoles = document.querySelector("select");
 const firstFieldSet = document.querySelectorAll("fieldset")[0];
 const InputOther = document.querySelector("#other-title");
 const designSelectMenu = document.querySelector("#design");
+const color = document.getElementById("color");
 const colorOptions = document.querySelectorAll("#color option");
 
 window.addEventListener("load", () => {
   focusNameInput();
-  hideColor();
 });
 
 // This function adds focus when page loads
@@ -29,30 +29,78 @@ function onOtherJobSelected(e) {
 }
 JobRoles.onchange = onOtherJobSelected;
 
+// function appendSelectOption() {
+//   const selectOption = document.createElement("option");
+//   selectOption.value = "Please Select";
+//   selectOption.text = "Select One Color";
+//   selectOption.setAttribute("disabled", true);
+//   color.insertBefore(selectOption, colorOptions[0]);
+//   reset();
+// }
+
+// function reset() {
+//   color.selectedIndex = 0;
+// }
+
 //change colors as the user selects the menu
 // Hide all the options
 function hideColor() {
+  // appendSelectOption();
   for (let i = 0; i < colorOptions.length; i++) {
     colorOptions[i].style.display = "none";
   }
 }
 
+function resetOptions() {
+  const size = document.querySelector("#size");
+  designSelectMenu.selectedIndex = 0;
+  size.selectedIndex = 0;
+}
+
+//calling functions
+hideColor();
+resetOptions();
 designSelectMenu.onchange = showAvailableColors;
+// Show available color on selection function
 function showAvailableColors(e) {
   // if theme js pune selected then show "cornflower blue, Dark slate grey, and gold "
   if (e.target.value === "js puns") {
     colorOptions.forEach(
       (color, i) => (color.style.display = i >= 0 && i < 3 ? "block" : "none")
     );
+    color.selectedIndex = 0; // Reset the options on change
+
     // if theme I JS selected show Tomoto, steel blue and dim grey
   } else if (e.target.value === "heart js") {
     colorOptions.forEach(
       (color, i) => (color.style.display = i <= 2 ? "none" : "block")
     );
+    color.selectedIndex = 3; // Reset the options on change
   }
 }
 
 // Function to register for activites
-const allCheckBoxes = document.querySelector("input[name = 'js-frameworks']");
+const activities = document.querySelector(".activities");
+const checkBoxes = document.querySelectorAll("input[type=checkbox]");
+console.log(checkBoxes);
 
-const checkBox1 = this.form.elements["express"];
+function appendTotal() {
+  const totalLabel = document.createElement("label");
+  const totalInput = document.createElement("input");
+  totalLabel.textContent = `Total: ${totalInput.value}`;
+
+  activities.appendChild(totalLabel);
+}
+
+function activity() {
+  appendTotal();
+  for (let i = 0; i < checkBoxes.length; i++) {
+    console.log(checkBoxes[i]);
+    checkBoxes[0].onchange = () =>
+      checkBoxes[1]
+        ? checkBoxes[1].setAttribute("disabled", true)
+        : checkBoxes[1].setAttribute("disabled", false);
+  }
+}
+
+activity();
